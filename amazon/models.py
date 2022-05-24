fd2815cb0d7f63992c63be400b7c5464cbd0e101
kubaslawski@gmail.com <utd1902glory>
2022-05-24 15:06:01 +0200

    3. Creating models. Applying default db in settings. Creating additional urls file.

--- amazon/models.py
+++ amazon/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=512, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(max_length=512, null=True, blank=True)
+    stock = models.IntegerField(null=True, blank=True)
+    price = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        self.price = round(self.price, 2)
+        super(Product, self).save(*args, **kwargs)
--- server/settings.py
+++ server/settings.py
@@ -82,17 +82,16 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
-    'amazon': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        },
         'NAME': amazon_db_name,
         'USER': amazon_user,
         'PASSWORD': amazon_password,
         'HOST': db_host,
         'PORT': db_port
-    }
+    },
 }
 
 
